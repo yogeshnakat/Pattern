0b0a96be7ab9b717e497da07e237d574bdd13d51
Yogesh90 <yogshnakat@gmail.com>
2021-03-05 13:36:05 +0530

    first changes

---
+++ src/Pat.java
@@ -0,0 +1,39 @@
+
+public class Pat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i,j;
+		
+		// For first half diamond
+		for(i=1;i<=5;i++) {
+							for(j=1;j<=9;j++) {
+												if(j>=6-i && j<=4+i) {
+																		System.out.print(i);
+																	}
+												else {
+														System.out.print(" ");	
+													}
+												}
+							
+							System.out.println();	
+						}
+		
+		// For Second half diamond
+		for(i=2;i<=5;i++) {
+							for(j=1;j<=9;j++) {
+												if(j>=i && j<=10-i) {
+																		System.out.print((6-i));
+																	}
+												else {
+														System.out.print(" ");	
+													}
+												}
+			
+							System.out.println();	
+						}
+
+
+						}
+				}
